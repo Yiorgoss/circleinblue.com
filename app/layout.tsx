63de1abd54525ff9a592d11b99fff08179d01558
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2023-09-30 16:59:28 +0300

    updated font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,24 @@
 import Providers from "@lib/providers";
 import "styles/globals.css";
 
+import { Playfair, } from 'next/font/google'
+
+const playfair = Playfair(
+{subsets:['latin'],
+variable:'--font-playfair'}
+)
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
   return (
     <html>
       <body>
         <Providers>
-          <main className="relative">{children}</main>
+          <main className={`${playfair.variable}`}>{children}</main>
         </Providers>
       </body>
     </html>
